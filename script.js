83abaa108a48f6f92c89c3e82bcb9f9721c030d5
Tazgaru <mauriciokrusser@gmail.com>
2022-06-26 14:05:21 -0500

    Add operate func

---
+++ script.js
@@ -0,0 +1,17 @@
+function operate(num1, num2, operator){
+
+    if (operator == '+'){
+        return num1 + num2;
+
+    }else if (operator =='-') {
+        return num1 - num2;
+
+    }else if (operator =='*') {
+        return num1 * num2;
+
+    }else if (operator == '/') {
+        return num1 / num2;
+    }else {
+        console.log('Unrecognized operator');
+    }
+}
